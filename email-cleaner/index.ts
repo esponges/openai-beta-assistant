3edd1200363dd3a1591aa109b3b92d7f3920ba3a
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-12-01 16:36:30 -0600

    add keepCleaning loop

--- email-cleaner/index.ts
+++ email-cleaner/index.ts
@@ -4,10 +4,15 @@ import {
   listUnreadMessages,
   markAsRead,
 } from './email';
+import { Thread } from "openai/resources/beta/threads/threads";
 
 // import the required dependencies
 require('dotenv').config();
 const OpenAI = require('openai');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Create a OpenAI connection
 const apiKey = process.env.OPENAI_API_KEY;
@@ -60,6 +65,14 @@ const openai = new OpenAI({
 //   }
 // };
 
+async function askQuestion(question: string) {
+  return new Promise<string>((resolve, _reject) => {
+    readline.question(question, (answer: string) => {
+      resolve(`${answer}\n`);
+    });
+  });
+}
+
 type Message = {
   id: string;
   snippet: string;
@@ -93,7 +106,7 @@ async function spamMessageFilter(messages: Message[], auth) {
   const readIds = messages
     .filter((message) => !message.is_spam_or_marketing)
     .map((message) => message.id);
-  
+
   // mark as read
   markAsRead(auth, readIds)
     .then(() => console.log('messages marked as read', readIds))
@@ -108,14 +121,6 @@ async function fetchLatestUnreadEmails(quantity: number = 10, auth) {
 }
 
 async function main() {
-  const gmailAuth = await initGmailAuth();
-  const messages = await fetchLatestUnreadEmails(10, gmailAuth);
-  if (Array.isArray(messages) && messages.length === 0) {
-    console.log('Failed to fetch messages');
-    return;
-  }
-  console.log('messages', messages);
-
   // use the assistant created using the OpenAI playground
   const assistantId = process.env.OPENAI_SPAM_FILTER_ASSISTANT_ID;
 
@@ -124,90 +129,118 @@ async function main() {
   }
   const assistant = await openai.beta.assistants.retrieve(assistantId);
 
-  // Create a thread
-  const thread = await openai.beta.threads.create();
-  console.log('starting thread with id: ', thread.id);
-
   // Log the first greeting
   console.log(
     "\nHello there, I'm your personal spam filter. I'll help you filter your emails.\n"
   );
 
-  const messagesAsString = JSON.stringify(messages);
-  await openai.beta.threads.messages.create(thread.id, {
-    role: 'user',
-    // todo?: when not passed as markdown, the assistant will not be able to parse the json
-    content: `\`\`\`json\n${messagesAsString}\n\`\`\``,
-  });
-  console.log('thinking...');
+  let keepCleaning = true;
+  let threadId: string;
+  while(keepCleaning) {
+    // fetch the latest unread emails
+    const gmailAuth = await initGmailAuth();
 
-  // Use runs to wait for the assistant response and then retrieve it
-  const run = await openai.beta.threads.runs.create(thread.id, {
-    assistant_id: assistant.id,
-  });
-
-  let actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
-
-  // Polling mechanism to see if actualRun is completed
-  // This should be made more robust.
-  while (
-    actualRun.status === 'queued' ||
-    actualRun.status === 'in_progress' ||
-    actualRun.status === 'requires_action'
-  ) {
-    // requires_action means that the assistant is waiting for the functions to be invoked
-    // the model should provide a json object with the shape of the json objects above
-    if (actualRun.status === 'requires_action') {
-      // extra single tool call
-      const toolCall =
-        actualRun.required_action?.submit_tool_outputs?.tool_calls[0];
-
-      const name = toolCall?.function.name;
-
-      const args = JSON.parse(toolCall?.function?.arguments || '{}');
-      const messages = args.messages;
-
-      if (name === 'spam_message_filter') {
-        await spamMessageFilter(messages, gmailAuth);
-      } else {
-        throw new Error('Unknown function name');
-      }
-
-      // we must submit the tool outputs to the run to continue
-      await openai.beta.threads.runs.submitToolOutputs(thread.id, run.id, {
-        tool_outputs: [
-          {
-            tool_call_id: toolCall?.id,
-            output: JSON.stringify({ success: true }),
-          },
-        ],
-      });
+    // todo: ask email quantity
+    const messages = await fetchLatestUnreadEmails(10, gmailAuth);
+    if (Array.isArray(messages) && messages.length === 0) {
+      console.log('Failed to fetch messages');
+      return;
     }
-    // keep polling until the run is completed
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+    console.log('messages', messages);
+  
+    // Create a thread
+    let thread: Thread;
+    if (!threadId) {
+      thread = await openai.beta.threads.create();
+    } else {
+      thread = await openai.beta.threads.retrieve(threadId);
+    }
+    threadId = thread.id;
+
+    console.log('starting thread with id: ', threadId);
+  
+    const messagesAsString = JSON.stringify(messages);
+    await openai.beta.threads.messages.create(thread.id, {
+      role: 'user',
+      // todo?: when not passed as markdown, the assistant will not be able to parse the json
+      content: `\`\`\`json\n${messagesAsString}\n\`\`\``,
+    });
+    console.log('thinking...');
+  
+    // Use runs to wait for the assistant response and then retrieve it
+    const run = await openai.beta.threads.runs.create(thread.id, {
+      assistant_id: assistant.id,
+    });
+  
+    let actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+  
+    // Polling mechanism to see if actualRun is completed
+    // This should be made more robust.
+    while (
+      actualRun.status === 'queued' ||
+      actualRun.status === 'in_progress' ||
+      actualRun.status === 'requires_action'
+    ) {
+      // requires_action means that the assistant is waiting for the functions to be invoked
+      // the model should provide a json object with the shape of the json objects above
+      if (actualRun.status === 'requires_action') {
+        // extra single tool call
+        const toolCall =
+          actualRun.required_action?.submit_tool_outputs?.tool_calls[0];
+  
+        const name = toolCall?.function.name;
+  
+        const args = JSON.parse(toolCall?.function?.arguments || '{}');
+        const messages = args.messages;
+  
+        if (name === 'spam_message_filter') {
+          await spamMessageFilter(messages, gmailAuth);
+        } else {
+          throw new Error('Unknown function name');
+        }
+  
+        // we must submit the tool outputs to the run to continue
+        await openai.beta.threads.runs.submitToolOutputs(thread.id, run.id, {
+          tool_outputs: [
+            {
+              tool_call_id: toolCall?.id,
+              output: JSON.stringify({ success: true }),
+            },
+          ],
+        });
+      }
+      // keep polling until the run is completed
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      actualRun = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+    }
+  
+    // Get the last assistant message from the messages array
+    const assisstantMessages = await openai.beta.threads.messages.list(thread.id);
+  
+    // Find the last message for the current run
+    const lastMessageForRun = assisstantMessages.data
+      .filter(
+        (message) => message.run_id === run.id && message.role === 'assistant'
+      )
+      .pop();
+  
+    // If an assistant message is found, console.log() it
+    if (lastMessageForRun) {
+      // aparently this is not correctly typed
+      // content returns an of objects do contain a text object
+      const messageValue = lastMessageForRun.content[0] as {
+        text: { value: string };
+      };
+  
+      console.log(messageValue?.text?.value);
+    }
+
+    const answer = await askQuestion('Do you want to continue? (y/n) ');
+    if (answer.startsWith('n')) {
+      keepCleaning = false;
+    }
   }
 
-  // Get the last assistant message from the messages array
-  const assisstantMessages = await openai.beta.threads.messages.list(thread.id);
-
-  // Find the last message for the current run
-  const lastMessageForRun = assisstantMessages.data
-    .filter(
-      (message) => message.run_id === run.id && message.role === 'assistant'
-    )
-    .pop();
-
-  // If an assistant message is found, console.log() it
-  if (lastMessageForRun) {
-    // aparently this is not correctly typed
-    // content returns an of objects do contain a text object
-    const messageValue = lastMessageForRun.content[0] as {
-      text: { value: string };
-    };
-
-    console.log(messageValue?.text?.value);
-  }
 }
 
 main().catch(console.error);
