a5b2842b0db61032d0d73c77b269f28e25cf23a6
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-09-07 13:31:22 -0600

    both with predef tags or not

--- tests/transcriptions.ts
+++ tests/transcriptions.ts
@@ -1,72 +1,63 @@
-import * as fs from "fs";
-import OpenAI from "openai";
-import { z } from "zod";
-import { zodFunction } from "openai/helpers/zod";
+import * as fs from 'fs';
+import OpenAI from 'openai';
+import { z } from 'zod';
+import { zodFunction } from 'openai/helpers/zod';
+import { type ChatCompletionMessageParam } from 'openai/resources/chat';
+
 require('dotenv').config();
-import { ChatCompletionMessageParam } from "openai/resources/chat";
 
 const openai = new OpenAI();
 
-async function main() {
+async function main(usePredefTags = true) {
   const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream("./assets/the-role-of-community.mp3"),
-    model: "whisper-1",
+    file: fs.createReadStream('./assets/the-role-of-community.mp3'),
+    model: 'whisper-1',
   });
 
-  if (!transcription.text) throw new Error("No text found");
+  if (!transcription.text) throw new Error('No text found');
 
   console.log(transcription.text);
 
-  // output:
-  // Edith wasn't getting any help from anyone. She had just taken the state realty exam, which was a lengthy multiple-choice exam in downtown Los Angeles. The testing room in the government building was freezing. As she hunched up shivering, she looked around and saw others feeling equally cold. Why couldn't these people let us know beforehand to bring jackets, she wondered. After getting the news that she had passed the test, she signed up with a realty company. They told her how much money she would owe them each month and wished her good luck. Other than that, they weren't much help or encouragement. They told her how to have open houses on weekends. She had to carry big signs in her car and place them in the neighborhood around the open house. The signs were not always easy to push into the ground, nor did they always stay upright. To make matters worse, one Saturday evening, she discovered that all of her signs had been stolen. Her realty company told her not to worry. They would just add the cost of replacing the signs to her monthly bill. After she had asked him many times, her husband finally accompanied her to an open house. They ended up spending eight hours together with each other that Sunday. Only a dozen house hunters showed up all day. She tried to engage Edgar in conversation, but all he wanted to do was read the newspaper. After he helped her pick up all the signs, he told her that was the last time he was going to help her on an open house. She asked why. He said he didn't want to talk about it. Edith wondered how long her realty career was going to last
-
-  // store in DBs
-  // real text https://eslyes.com/eslread/ss/s065.htm
-
-  // tags
-  // Compassion
-  // Forgiveness
-  // Gratitude
-  // Growth
-  // Service
-  // Stewardship
-  // Honesty
-  // Dignity
-  // Peace
-  // Wisdom
-
   const orderParameters = z.object({
-    tags: z.array(z.string()).describe("the main tags from the provided text"),
+    tags: z.array(z.string()).describe('the main tags from the provided text'),
   });
 
-  const tools = [
-    zodFunction({ name: "getTags", parameters: orderParameters })
-  ];
+  const tools = [zodFunction({ name: 'getTags', parameters: orderParameters })];
+
+  const prompt = usePredefTags
+    ? `You are a helpful tag generator assistant. Given a text, you will return the main tags that could
+    help a content creator to group the content in a better way. Here are the main guidelines:
+
+    - You can only use the following tags: Compassion, Forgiveness, Gratitude, Growth, Service, Stewardship, Honesty, Dignity, Peace, Wisdom
+    - If you don't think any of the tags are relevant to the text, just return an empty array of tags
+    `
+    : `You are a helpful tag generator assistant. Given a text, you will return the main tags that could
+    help a content creator to group the content in a better way.`;
 
   const messages: Array<ChatCompletionMessageParam> = [
     {
-      role: "assistant",
-      content:
-        `You are a helpful tag generator assistant. Given a text, you will return the main tags. Here are the main guidelines:
-        
-        - You can only use the following tags: Compassion, Forgiveness, Gratitude, Growth, Service, Stewardship, Honesty, Dignity, Peace, Wisdom
-        - If you don't think any of the tags are relevant to the text, just return an empty array of tags
-        `,
-      },
+      role: 'assistant',
+      content: prompt,
+    },
 
     {
-      role: "user",
+      role: 'user',
       content: transcription.text,
     },
   ];
 
   const response = await openai.chat.completions.create({
-    model: "gpt-4o-2024-08-06",
+    model: 'gpt-4o-2024-08-06',
     messages: messages,
     tools: tools,
     stream: false,
   });
-  
+
   console.log(JSON.stringify(response, null, 2));
 }
-main();
+
+const withPredefTags = process.argv.includes('--with-predefined-tags');
+
+main(withPredefTags)
+  .then(() => console.log('Done'))
+  .catch(console.error);
